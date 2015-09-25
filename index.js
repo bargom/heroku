14ebe8a5cb8d481a3534a630cbe7fd7e516fa1ae
bargom <baris.gomlek@gmail.com>
2015-09-25 13:40:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World! I am running on heroku. <a href="barisFBTest://auth/#state=123456789">Click here.</a>');
+  response.send('<h1>Hello World! I am running on heroku. <a href="barisFBTest://auth/#state=123456789">Click here for barisFBTest</a><br /><a href="bean_realm123://auth/#state=123456789">Click here for bean_realm123</a></h1>');
 });
 
 app.listen(app.get('port'), function() {
