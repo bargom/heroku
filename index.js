23d004cc4ae7fc5e94735e673df160f0ad0efb7f
bargom <baris.gomlek@gmail.com>
2015-10-01 15:48:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('<h1>Hello World! I am running on heroku. <a href="barisFBTest://auth/#state=123456789">Click here for barisFBTest</a><br /><a href="beanrealm123://auth/#state=123456789">Click here for beanrealm123</a><a href="bean-realm.pro.nl-be://auth/#state=123456789">Click here for bean-realm.pro.nl-be</a></h1>');
+  response.send('<h1>Hello World! I am running on heroku. <a href="barisFBTest://auth/#state=123456789">Click here for barisFBTest</a><br /><a href="beanrealm123://auth/#state=123456789">Click here for beanrealm123</a><br /><a href="bean-realm.pro.nl-be://auth/#state=123456789">Click here for bean-realm.pro.nl-be</a></h1>');
 });
 
 app.listen(app.get('port'), function() {
